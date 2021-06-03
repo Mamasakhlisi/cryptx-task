f871841bf342627fe3347b0e07c8544b5822ef9d
Mamasakhlisi <datomamasakhlisi@gmail.com>
2021-06-03 06:30:17 -0700

    feat: create node

---
+++ src/redux/tree/treeReducer.js
@@ -0,0 +1,102 @@
+export const getAllChildrens = (child) => {
+  return { type: "GET", payload: child };
+};
+
+export const createChildren = (key) => {
+  return function (dispatch, getState) {
+    const { data } = getState().tree;
+
+    function countDown(fromNumber) {
+      fromNumber.forEach((element) => {
+        if (element.parent === 1) {
+          if (element.key === "0-1-2") {
+            console.log(element);
+            element.children.push({
+              title: "new file",
+              key: "0-1-4",
+              isLeaf: true,
+              parent: 0,
+            });
+          }
+          countDown(element.children);
+        }
+      });
+    }
+    const dt = countDown(data);
+    return dispatch({ type: "NEW", payload: dt });
+  };
+};
+
+const initialState = {
+  childrens: [],
+  data: [
+    {
+      title: "დათოს ფოლდერი",
+      key: "0",
+      isLeaf: false,
+      parent: 1,
+      children: [
+        {
+          title: "File Name",
+          key: "0-0",
+          isLeaf: true,
+          parent: 0,
+        },
+        {
+          title: "დათოს ფოტოები",
+          key: "0-1",
+          isLeaf: false,
+          parent: 1,
+          children: [
+            {
+              title: "File Name",
+              key: "0-1-1",
+              isLeaf: true,
+              parent: 0,
+            },
+            {
+              title: "დათო",
+              key: "0-1-2",
+              isLeaf: false,
+              parent: 1,
+              children: [
+                {
+                  title: "File Name",
+                  key: "0-1-3",
+                  isLeaf: true,
+                  parent: 0,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "დათო2 ფოლდერი",
+      key: "1",
+      isLeaf: false,
+      parent: 1,
+      children: [
+        {
+          title: "File Name",
+          key: "1-0",
+          isLeaf: true,
+          parent: 0,
+        },
+      ],
+    },
+  ],
+};
+
+export const treeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET":
+      return { ...state, childrens: [...state.childrens, action.payload] };
+    case "NEW":
+      return state;
+
+    default:
+      return state;
+  }
+};
